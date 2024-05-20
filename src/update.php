332d5b32e7b81e191cde72bdf6e1236426f5fe11
frdrckj <frederickjerusha@gmail.com>
2024-05-21 05:31:30 +0700

    modified

--- src/update.php
+++ src/update.php
@@ -102,7 +102,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
 
 </head>
 <body>
-
+<?php include 'navbar.php'; ?>
 <?php
 if (isset($message)) {
     foreach ($message as $msg) {
